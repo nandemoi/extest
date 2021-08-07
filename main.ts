63940df22d14d91729c3ce807fccf6f99be55c0d
nandemoi <nandemouda@gmail.com>
2021-08-07 20:12:37 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace test {
+    /**
+     * Address 
+     */
+    //% blockId=testext
+    //% block="plot at $index"
+    //% index.min=0 index.max=25
     export function test (index: number): void {
         const y = Math.floor(index / 5);
         const x = Math.floor(index % 5);
