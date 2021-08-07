4928eed4f7b2b36b5fa76c768451f32b05b2a7f8
nandemoi <nandemouda@gmail.com>
2021-08-07 20:10:40 +0800

    更新 main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
-})
+namespace test {
+    export function test (index: number): void {
+        const y = Math.floor(index / 5);
+        const x = Math.floor(index % 5);
+        led.plot (x, y)
+    }
+}
